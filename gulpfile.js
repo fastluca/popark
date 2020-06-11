66d95325f78b8e56b12e92d4a17a4f8a0a8c78ba
Gianluca Colaianni <fast_luca@hotmail.it>
2020-06-11 17:10:00 +0200

    Fix gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,12 @@
 var gulp = require('gulp');
 var sitemap = require('gulp-sitemap');
  
-gulp.task('sitemap', function () {
+gulp.task('sitemap', async function () {
     gulp.src('index.html', {
             read: true
         })
         .pipe(sitemap({
             siteUrl: 'https://popark.it'
         }))
-        .pipe(gulp.dest('./'));
+        .pipe(gulp.dest('./'))
 });
